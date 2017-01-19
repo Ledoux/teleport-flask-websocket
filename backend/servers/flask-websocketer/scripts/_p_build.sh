0209737eb5e694006863fb6f4e7116064dd8b979
franblas <francois_blas@hotmail.com>
2017-01-19 14:20:32 +0100

    refactor: add heroku scripts and fix placeholders. Rename folder into backend/server

--- backend/servers/flask-websocketer/scripts/_p_build.sh
+++
@@ -1,1 +0,0 @@
-docker -H tcp://$(docker.host):$(docker.port) build -f $(type.name)_Dockerfile -t $(registry.host):$(registry.port)/$(run.tag):$(current.version) $BUILD_OPTIONS .
--- backend/servers/flask-websocketer/scripts/_p_localhost_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -d "../../../$(project.config.venv)" ] ; then
-  source ../../../$(project.config.venv)/bin/activate
-fi
-if [ "$MODE" != "localhost" ] ; then
-  $(manageExtraConfig) gunicorn -H ../../../$(project.config.venv)  --config config/localhost_guwsgi.ini app:app
-else
-  $(manageExtraConfig) python scripts/manage.py
-fi
