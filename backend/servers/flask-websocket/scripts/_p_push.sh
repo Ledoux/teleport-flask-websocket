6c2b93ca87717ab3b70d7a6f63f002044147726b
Ledoux <erwan.ledoux@snips.net>
2017-01-23 15:16:53 +0100

    fix the kubernetes config

--- backend/servers/flask-websocket/scripts/_p_push.sh
+++ backend/servers/flask-websocket/scripts/_p_push.sh
@@ -1,10 +1,14 @@
-f $[docker.tcp]; then
+if $[docker.tcp]; then
 	docker -H tcp://$[docker.host]:$[docker.port] push $[registry.url]/$[run.tag]:$[current.version]
 else
 	docker push $[registry.url]/$[run.tag]:$[current.version]
 fi
 cd ../../../
 for suffix in service controller; do \
+	export KUBERNETES_RETHINK_HOST=$[kubernetes.rethink.host] && \
+	export KUBERNETES_RETHINK_PORT=$[kubernetes.rethink.port] && \
+	export KUBERNETES_RETHINK_DATABASE=$[kubernetes.rethink.database] && \
+	export KUBERNETES_RETHINK_TABLE=$[kubernetes.rethink.table] && \
 	tpt -e --lang py --method register,backend/servers/$[server.name]/config/$[type.name]_$suffix.yaml; \
 done
 cd backend/servers/$[server.name]
--- backend/servers/flask-websocket/scripts/_p_run.sh
+++ backend/servers/flask-websocket/scripts/_p_run.sh
@@ -1,3 +1,8 @@
-cd ../../../ && tpt -e --lang py --method restart,$[run.tag]
+cd ../../../ &&
+export KUBERNETES_RETHINK_HOST=$[kubernetes.rethink.host] && \
+export KUBERNETES_RETHINK_PORT=$[kubernetes.rethink.port] && \
+export KUBERNETES_RETHINK_DATABASE=$[kubernetes.rethink.database] && \
+export KUBERNETES_RETHINK_TABLE=$[kubernetes.rethink.table] && \
+tpt -e --lang py --method restart,$[run.tag]
 cd backend/servers/$[server.name]
 echo Your service is available here: $[run.url]
