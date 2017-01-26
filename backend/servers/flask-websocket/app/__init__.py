41d971f612fcb017ac2753636ebf95f1281a4167
franblas <francois_blas@hotmail.com>
2017-01-26 15:04:23 +0100

    clean app

--- backend/servers/flask-websocket/app/__init__.py
+++ backend/servers/flask-websocket/app/__init__.py
@@ -1,15 +1,9 @@
 #
 # IMPORTS
 #
-import sys
-from flask import Blueprint,Flask,flash,g,render_template
+from flask import Flask,render_template
 from flask_socketio import SocketIO,send,emit
 import os
-import hmac
-import json
-server_dir = '/'.join(__file__.split('/')[:-1])
-sys.path.append(os.path.join(server_dir, 'lib'))
-sys.path.append(os.path.join(server_dir, 'modules'))
 
 #
 # FLASK
@@ -33,7 +27,6 @@ def test_ping(*args):
 #
 default = {
     'DATA': "localhost",
-    'SMTP_HOST': "localhost",
     'SITE_NAME': "",
     'TYPE': "localhost",
     'WEB': "on",
@@ -46,28 +39,6 @@ for couples in default.items():
 		couples[1]
 	)
 app.config['HOST_DIR'] = "./" if app.config['TYPE'] != 'localhost' else os.path.join(os.getcwd().split('backend')[0], 'backend/')
-
-#
-# SECRET
-#
-config_dir = './config'
-secret_dir = os.path.join(config_dir, 'secret.json')
-with open(secret_dir, 'r') as file:
-    app.config.update(json.load(file))
-
-#
-# DATABASES
-#
-app.config['databases_by_name'] = {}
-
-#
-# CLIENT SECRET
-#
-client_secret_name = app.config['TYPE'] + '_client_secret.json'
-client_secret_path = os.path.join(config_dir, client_secret_name)
-if os.path.isfile(client_secret_path):
-    with open(client_secret_path,'r') as cl_se:
-        app.config["GOOGLE_CLIENT_ID"] = json.load(cl_se)['web']['client_id']
 
 #
 # FLASK ENV
@@ -98,96 +69,6 @@ def get_ping():
 @app.route('/<path:path>')
 def get_home_redirect(path):
 	return get_home()
-
-#
-# LOGIN
-#
-# if the module private is not inside the module folder,
-# then the login manager of flask is not called to be set.
-# By a certain non explained almost magical reason, this fix the property of
-# the flask to render static files.
-is_login = os.path.isfile('./app/modules/private.py')
-if is_login:
-    from flask.ext.login import LoginManager,current_user
-    login_manager = LoginManager(app)
-    login_manager.login_view = 'private.login_index'
-    @app.before_request
-    def add_user_before_request():
-        g.user = current_user
-
-#
-# BLUEPRINTS
-#
-# then we can also go to specific modules inside this webserver
-modules_path = os.path.join(server_dir, 'modules')
-if os.path.isdir(modules_path):
-    sys.path.append(modules_path)
-    module_names = map(
-        lambda fcouples:
-        fcouples[0],
-        filter(
-            lambda couples:
-            couples[-1]=='py' and couples[0] != "__init__",
-            map(
-                lambda file_name:
-                file_name.split('.'),
-                filter(
-                    lambda dir_name:
-                    dir_name[0] != ".",
-                    os.listdir(modules_path)
-                )
-            )
-        )
-    )
-else:
-    module_names = []
-# and also it specific libs
-lib_path = os.path.join(server_dir, 'lib')
-if os.path.isdir(lib_path):
-    sys.path.append(lib_path)
-    lib_names = map(
-        lambda fcouples:
-        fcouples[0],
-        filter(
-            lambda couples:
-            couples[-1]=='py' and couples[0] != "__init__",
-            map(
-                lambda file_name:
-                file_name.split('.'),
-                filter(
-                    lambda dir_name:
-                    dir_name[0] != ".",
-                    os.listdir(lib_path)
-                )
-            )
-        )
-    )
-    for lib_name in lib_names:
-        __import__(lib_name)
-# once the modules and libs are imported we can now
-# also plug the blueprints flask to the app
-map(
-    lambda module:
-    app.register_blueprint(getattr(module,'blueprint')),
-    filter(
-        lambda module:
-        hasattr(module,'blueprint'),
-        map(
-            lambda module_name:
-            __import__(module_name),
-            module_names
-        )
-    )
-)
-
-#
-# TABLES
-#
-app.config['tables_by_name'] = {}
-for database_name, database in app.config['databases_by_name'].items():
-    for table in database['tables']:
-        table['database_name'] = database_name
-        app.config['tables_by_name'][table['name']] = table
 
 #
 # RUN
