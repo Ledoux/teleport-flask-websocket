ffbfc859d05c2f97b3ee5b259bb64eb3ca95da9c
erwanledoux <lawanledoux@gmail.com>
2017-01-31 18:28:17 +0100

    remove proposition to delete heroku app

--- backend/servers/flask-websocket/scripts/_p_heroku_build.sh
+++ backend/servers/flask-websocket/scripts/_p_heroku_build.sh
@@ -15,14 +15,4 @@ else
     fi
   fi
   echo "$[run.subDomain] has been already created"
-  echo "Do you wish to delete this app (y/n)?"
-  read answer
-  if echo "$answer" | grep -iq "^y"; then
-    heroku apps:destroy $[run.subDomain] --confirm $[run.subDomain];
-    git init
-    heroku create --app $[run.subDomain] --buildpack heroku/python --remote $[type.name];
-    heroku config:set --app $[run.subDomain] TYPE=$[type.name];
-  else
-    echo No
-  fi
 fi
