81a3b2c06e242ea77ff35c536ed7380c2f073410
erwanledoux <lawanledoux@gmail.com>
2017-02-01 16:48:53 +0100

    better set VIRTUAL_ENV

--- backend/servers/flask-websocket/scripts/_p_localhost_start.sh
+++ backend/servers/flask-websocket/scripts/_p_localhost_start.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-if [ -d "../../../$[project.config.venv]" ] ; then
-  source ../../../$[project.config.venv]/bin/activate
+if [ "$VIRTUAL_ENV" != "" ] ; then
+  VIRTUAL_ENV_OPTION = -H $VIRTUAL_ENV
+else
+  VIRTUAL_ENV_OPTION = ""
 fi
 if [ "$MODE" != "localhost" ] ; then
-  $[manageExtraConfig] gunicorn -H ../../../$[project.config.venv]  --config config/localhost_guwsgi.ini app:app
+  $[manageExtraConfig] gunicorn $VIRTUAL_ENV_OPTION --config config/localhost_guwsgi.ini app:app
 else
   $[manageExtraConfig] python scripts/manage.py
 fi
