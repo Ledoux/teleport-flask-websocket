5181d26b3479c6a7c1bf6e44f055a1260b941eb8
erwanledoux <lawanledoux@gmail.com>
2017-02-01 17:05:18 +0100

    fixed typo in VIRTUAL_ENV_OPTION

--- backend/servers/flask-websocket/scripts/_p_localhost_start.sh
+++ backend/servers/flask-websocket/scripts/_p_localhost_start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ "$VIRTUAL_ENV" != "" ] ; then
-  VIRTUAL_ENV_OPTION = -H $VIRTUAL_ENV
+  VIRTUAL_ENV_OPTION="-H $VIRTUAL_ENV"
 else
-  VIRTUAL_ENV_OPTION = ""
+  VIRTUAL_ENV_OPTION=""
 fi
 if [ "$MODE" != "localhost" ] ; then
   $[manageExtraConfig] gunicorn $VIRTUAL_ENV_OPTION --config config/localhost_guwsgi.ini app:app
